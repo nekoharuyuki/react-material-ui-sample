9869e1ad2e24454678a43cae6dfb20b64e5e79ed
nekoharuyuki <qqts7nbd@gmail.com>
2023-08-13 12:12:12 +0900

    ログイン画面追加

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import ProductPage from "./components/pages/ProductPage";
 import HomePage from "./components/pages/HomePage";
+import SignInPage from "./components/pages/SignInPage";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Switch>
         <Route path="/products" component={ProductPage} exact />
-        <Route path="/" component={HomePage} exact />
+        <Route path="/Home" component={HomePage} exact />
+        <Route path="/" component={SignInPage} exact />
       </Switch>
     </Router>
   );
--- src/components/templates/GenericTemplate.tsx
+++ src/components/templates/GenericTemplate.tsx
@@ -64,11 +64,12 @@ const AppBar = styled(MuiAppBar, {
 const Copyright = () => {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" to="/">
-        管理画面
-      </Link> {new Date().getFullYear()}
-      {"."}
+      {'Copyright © '}
+      <Link color="inherit" to="https://nekoharuyuki.github.io/Privacy.Policy.io/">
+        Neko Haruyuki
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
     </Typography>
   );
 };
@@ -192,7 +193,7 @@ const GenericTemplate: React.FC<GenericTemplateProps> = ({
           </Toolbar>
           <Divider />
           <List component="nav">
-            <Link to="/">
+            <Link to="/Home">
               <ListItem>
                 <ListItemIcon>
                   <HomeIcon />
