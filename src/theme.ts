2f239cc103450c66f05a6f9bdcba26e9b84fa5e4
nekoharuyuki <qqts7nbd@gmail.com>
2023-10-12 23:36:43 +0900

    theme調整

--- src/theme.ts
+++ src/theme.ts
@@ -1,3 +1,274 @@
-import { createTheme } from "@mui/material/styles";
+import { createContext, useState, useMemo } from "react";
+import { createTheme, Theme } from "@mui/material/styles";
 
-export const theme = createTheme({});
+// カラーデザイントークンの型
+type ColorTokens = {
+  [key: string]: {
+    [key: number]: string;
+  };
+};
+
+// カラーデザイントークンを生成
+const tokens = (mode: "dark" | "light"): ColorTokens => {
+  // カラーデザイントークンの定義
+  const designTokens: ColorTokens = {
+    grey: {
+      100: "#e0e0e0",
+      200: "#c2c2c2",
+      300: "#a3a3a3",
+      400: "#858585",
+      500: "#666666",
+      600: "#525252",
+      700: "#3d3d3d",
+      800: "#292929",
+      900: "#141414",
+    },
+    primary: {
+      100: "#d0d1d5",
+      200: "#a1a4ab",
+      300: "#727681",
+      400: "#1F2A40",
+      500: "#141b2d",
+      600: "#101624",
+      700: "#0c101b",
+      800: "#080b12",
+      900: "#040509",
+    },
+    greenAccent: {
+      100: "#dbf5ee",
+      200: "#b7ebde",
+      300: "#94e2cd",
+      400: "#70d8bd",
+      500: "#4cceac",
+      600: "#3da58a",
+      700: "#2e7c67",
+      800: "#1e5245",
+      900: "#0f2922",
+    },
+    redAccent: {
+      100: "#f8dcdb",
+      200: "#f1b9b7",
+      300: "#e99592",
+      400: "#e2726e",
+      500: "#db4f4a",
+      600: "#af3f3b",
+      700: "#832f2c",
+      800: "#58201e",
+      900: "#2c100f",
+    },
+    blueAccent: {
+      100: "#e1e2fe",
+      200: "#c3c6fd",
+      300: "#a4a9fc",
+      400: "#868dfb",
+      500: "#6870fa",
+      600: "#535ac8",
+      700: "#3e4396",
+      800: "#2a2d64",
+      900: "#151632",
+    },
+  };
+
+  // ダークモードの場合の設定
+  if (mode === "dark") {
+    designTokens.grey = {
+      100: "#e0e0e0",
+      200: "#c2c2c2",
+      300: "#a3a3a3",
+      400: "#858585",
+      500: "#666666",
+      600: "#525252",
+      700: "#3d3d3d",
+      800: "#292929",
+      900: "#141414",
+    };
+    designTokens.primary = {
+      100: "#d0d1d5",
+      200: "#a1a4ab",
+      300: "#727681",
+      400: "#1F2A40",
+      500: "#141b2d",
+      600: "#101624",
+      700: "#0c101b",
+      800: "#080b12",
+      900: "#040509",
+    };
+    designTokens.greenAccent = {
+      100: "#dbf5ee",
+      200: "#b7ebde",
+      300: "#94e2cd",
+      400: "#70d8bd",
+      500: "#4cceac",
+      600: "#3da58a",
+      700: "#2e7c67",
+      800: "#1e5245",
+      900: "#0f2922",
+    };
+    designTokens.redAccent = {
+      100: "#f8dcdb",
+      200: "#f1b9b7",
+      300: "#e99592",
+      400: "#e2726e",
+      500: "#db4f4a",
+      600: "#af3f3b",
+      700: "#832f2c",
+      800: "#58201e",
+      900: "#2c100f",
+    };
+    designTokens.blueAccent = {
+      100: "#e1e2fe",
+      200: "#c3c6fd",
+      300: "#a4a9fc",
+      400: "#868dfb",
+      500: "#6870fa",
+      600: "#535ac8",
+      700: "#3e4396",
+      800: "#2a2d64",
+      900: "#151632",
+    };
+  }
+
+  return designTokens;
+};
+
+// テーマ設定の型
+type ThemeSettings = {
+  palette: {
+    mode: "dark" | "light";
+    primary: {
+      main: string;
+    };
+    secondary: {
+      main: string;
+    };
+    neutral: {
+      dark: string;
+      main: string;
+      light: string;
+    };
+    background: {
+      default: string;
+    };
+  };
+  typography: {
+    fontFamily: string;
+    fontSize: number;
+    h1: {
+      fontFamily: string;
+      fontSize: number;
+    };
+    h2: {
+      fontFamily: string;
+      fontSize: number;
+    };
+    h3: {
+      fontFamily: string;
+      fontSize: number;
+    };
+    h4: {
+      fontFamily: string;
+      fontSize: number;
+    };
+    h5: {
+      fontFamily: string;
+      fontSize: number;
+    };
+    h6: {
+      fontFamily: string;
+      fontSize: number;
+    };
+  };
+};
+
+// テーマ設定を生成
+const themeSettings = (mode: "dark" | "light"): ThemeSettings => {
+  const colors = tokens(mode);
+  return {
+    palette: {
+      mode: mode,
+      ...(mode === "dark"
+        ? {
+            primary: {
+              main: colors.primary[500],
+            },
+            secondary: {
+              main: colors.greenAccent[500],
+            },
+            neutral: {
+              dark: colors.grey[700],
+              main: colors.grey[500],
+              light: colors.grey[100],
+            },
+            background: {
+              default: colors.primary[500],
+            },
+          }
+        : {
+            primary: {
+              main: colors.primary[100],
+            },
+            secondary: {
+              main: colors.greenAccent[500],
+            },
+            neutral: {
+              dark: colors.grey[700],
+              main: colors.grey[500],
+              light: colors.grey[100],
+            },
+            background: {
+              default: "#fcfcfc",
+            },
+          }),
+    },
+    typography: {
+      fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+      fontSize: 12,
+      h1: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 40,
+      },
+      h2: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 32,
+      },
+      h3: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 24,
+      },
+      h4: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 20,
+      },
+      h5: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 16,
+      },
+      h6: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 14,
+      },
+    },
+  };
+};
+
+// カラーモード切り替えのためのコンテキスト
+export const ColorModeContext = createContext({
+  toggleColorMode: () => {},
+});
+
+// カラーモードを切り替えるためのフック
+export const useMode = (): [Theme, { toggleColorMode: () => void }] => {
+  const [mode, setMode] = useState<"dark" | "light">("dark");
+
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () =>
+        setMode((prev) => (prev === "light" ? "dark" : "light")),
+    }),
+    []
+  );
+
+  const theme = useMemo(() => createTheme(themeSettings(mode)) as Theme, [mode]);
+
+  return [theme, colorMode];
+};
