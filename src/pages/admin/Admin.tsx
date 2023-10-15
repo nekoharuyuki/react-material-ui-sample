8733235b607cd04d895faf10f269f480f78d6978
nekoharuyuki <qqts7nbd@gmail.com>
2023-10-15 13:24:23 +0900

    ユーザー登録画面を追加

--- src/pages/admin/Admin.tsx
+++ src/pages/admin/Admin.tsx
@@ -5,17 +5,37 @@ import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Accounts from './Account';
+import SignUp from './signup/SignUp';
 
 export const Admin: FC = () => {
   return (
       <Box sx={{ display: 'flex' }}>
         <Toolbar />
         <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} >
+            <Paper
+              sx={{
+                p: 2,
+                display: 'flex',
+                flexDirection: 'column'
+              }}
+            >
+              <SignUp />
+            </Paper>
+          </Grid>
           <Grid item xs={12}>
-            <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+            <Paper
+              sx={{
+                p: 2,
+                display: 'flex',
+                flexDirection: 'column',
+              }}
+            >
               <Accounts />
             </Paper>
           </Grid>
+        </Grid>
         </Container>
       </Box>
   );
