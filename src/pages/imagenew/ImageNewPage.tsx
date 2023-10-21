f7b0c094e373844fa4e757102b44095735c27c49
nekoharuyuki <qqts7nbd@gmail.com>
2023-10-22 00:43:21 +0900

    ファイルアップロードボタン追加

--- src/pages/imagenew/ImageNewPage.tsx
+++ src/pages/imagenew/ImageNewPage.tsx
@@ -1,5 +1,61 @@
 import { FC } from "react";
+import Title from '../../components/templates/Title';
+import { storage } from "../../lib/firebase/firebase";
+import { useState, useEffect } from "react";
+import { Box, Drawer as LinearProgress, Typography } from "@mui/material";
 
 export const ImageNewPage: FC = () => {
-  return <div>画像新規作成ページ</div>;
+  const [image, setImage] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [error, setError] = useState("");
+  const [progress, setProgress] = useState(100);
+
+  const handleImage = (event) => {
+    const image = event.target.files[0];
+    setImage(image);
+    setError("");
+  };
+
+  const submit = (event) => {
+    event.preventDefault();
+    if (image === "") {
+      setError("ファイルが選択されていません");
+      return;
+    }
+  }
+
+  return (
+    <div>
+      <Title>画像新規作成ページ</Title>
+      <div>
+          {/* 画像アップロード用のフォーム */}
+          <form onSubmit={submit}>
+            <input type="file" onChange={handleImage} />
+            <button onClick={submit}>アップロード</button>
+          </form>
+      </div>
+      {progress !== 100 && <LinearProgressWithLabel value={progress} />}
+      {error}
+      {imageUrl && (
+        <div>
+          <img width="400px" src={imageUrl} alt="uploaded" />
+        </div>
+      )}
+    </div>
+  );
 };
+
+function LinearProgressWithLabel(props) {
+  return (
+    <Box display="flex" alignItems="center">
+      <Box width="100%" mr={1}>
+        <LinearProgress variant="determinate" {...props} />
+      </Box>
+      <Box minWidth={35}>
+        <Typography variant="body2" color="textSecondary">{`${Math.round(
+          props.value
+        )}%`}</Typography>
+      </Box>
+    </Box>
+  );
+}
